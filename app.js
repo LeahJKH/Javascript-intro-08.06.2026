06e1e5c43936eca064533479f8d5f5a9dc77cd96
LeahJKH <leahjb06.kodehode@gmail.com>
2026-06-08 12:31:48 +0200

    puttet inn kommentarer

--- app.js
+++ app.js
@@ -11,47 +11,52 @@ const obj = {
 
 let litteralstring = `hei jeg heter ${obj.navn}`;
 
-console.log(array[1])
-console.log(obj.alder
-    
-)
+// forskjellige typer variabler ^
 
-// if (number === 12) {
-//  console.log("tallet ditt er 12")
-// } else if (number === 13) {
+console.log(array[1]) // hente ut johanne fra array. arraay starter alltid på 0
+console.log(obj.alder) // hvordan hente ut objecter
 
-// } else {
-//  console.log("Tallet ditt er ikke 12 eller 13");
-// }
+
+
+if (number === 12) {
+ console.log("tallet ditt er 12")
+} else if (number === 13) {
+
+} else { // om over ikke er sann kjør dette
+ console.log("Tallet ditt er ikke 12 eller 13");
+}
+// conditionals
+
 
 function Start() {
     console.log("jeg er en funksjon");
-}
+} 
 
-// Start();
+Start(); // en "call" for en funksjon for og kjøre koden
 
-array.pop()
+array.pop() // en array metode med kode for og fjerne siste verdi i et array
 
-const section = document.querySelector("#mainCont")
+const section = document.querySelector("#mainCont") // hente et html element
 
-section.innerHTML = "<p>HEI PÅ DEI</p>"
+section.innerHTML = "<p>HEI PÅ DEI</p>" // en metode for og endre html
 
-// for (let index = 0; index <= 200; index++) {
-//     console.log(index)
-// }
+for (let index = 0; index <= 200; index++) {
+    console.log(index)
+} // for loop som kjører 200 ganger før den går videre
 
-// while (number <= 14) {
-//     console.log(number)
-// number++
-// }
+while (number <= 14) {
+    console.log(number)
+number++
+} // while loop. vær forsiktig med at while ikke kjører forf alltid dersom dette crasher browseren
 
+// operatører 
 // +, -, /, *, %, ++, --,
 // +=, -=, /=, *=
 
 console.log(13 % 2)
 
 
-
+// en event listner funksjon. hvor html "caller" funksjonen
 function consoleEvent() {
     console.log("Jeg har blitt klikket")
     const div = document.createElement("div")
@@ -63,13 +68,15 @@ function consoleEvent() {
     section.appendChild(div)
 }
 
+
+// en eventlistner fult i javascript
 const btn = document.querySelector("#clickEvent")
 const count = document.querySelector("#counter")
-let counted = 0
+let counted = 0 // en variabel vi kan endre
 btn.addEventListener("click", () => {
     console.log("jeg er den bedre knappen")
-    counted++
-    count.textContent = counted
+    counted++ // ++ er alltid +1 til variabelen
+    count.textContent = counted // vil endre texten i count elementet
     
 })
 
